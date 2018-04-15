49d7b41797634bca5a5b994583d7e36e8a207d38
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2018-04-14 23:05:00 -0400

    dont always stop

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,14 +2,13 @@ class GameController < ApplicationController
   def alexa
     input = AlexaRubykit.build_request(params)
     output = AlexaRubykit::Response.new
-    session_end = true
+    session_end = false
     message = "There was not an error." # unknown thing happened
 
     case input.type
     when "LAUNCH_REQUEST"
       # user talked to our skill but did not say something matching intent
       message = "Say something see what happens."
-      session_end = false
     when "INTENT_REQUEST"
       case input.name
       when "AMAZON.CancelIntent"
